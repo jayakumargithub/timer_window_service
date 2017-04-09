f7b6e406d39af881fd21bd75ed8b19ef79e920e4
Jayakumar Mogenahalli <jayakumar.m.h@gmail.com>
2017-04-09 09:34:57 +0100

    initial

---
+++ TimerWindowService/Service1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimerWindowService
+{
+    public partial class TimerSrv : ServiceBase
+    {
+        private WriteToFile file;
+        private Timer timer;
+        public TimerSrv()
+        {
+            file = new WriteToFile();
+            timer  = new Timer();
+            InitializeComponent();
+        }
+
+        private void OnTimeElapsed(object source, ElapsedEventArgs e)
+        {
+           file.Write("Time elapased!");
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            file.Write("Service Started");
+            timer.Elapsed += new ElapsedEventHandler(OnTimeElapsed);
+            //ad 2: set interval to 1 minute (= 60,000 milliseconds)
+
+            timer.Interval = 60000;
+
+            //ad 3: enabling the timer
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            timer.Enabled = false;
+            file.Write("Service Stoped");
+        }
+    }
+}
---
+++ TimerWindowService/WriteToFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerWindowService
+{
+    public class WriteToFile
+    {
+        public void Write(string contents)
+        {
+
+            //set up a filestream
+            FileStream fs = new
+       FileStream(@"C:\temp\log.txt", FileMode.OpenOrCreate, FileAccess.Write);
+
+            //set up a streamwriter for adding text
+
+            StreamWriter sw = new StreamWriter(fs);
+
+            //find the end of the underlying filestream
+
+            sw.BaseStream.Seek(0, SeekOrigin.End);
+
+            //add the text 
+            sw.WriteLine(contents);
+            //add the text to the underlying filestream
+
+            sw.Flush();
+            //close the writer
+            sw.Close();
+        }
+    }
+}
